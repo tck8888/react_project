322eb4c1d9c8009185a18934e80edf9a41865784
tck <tck@dra100.com>
2019-02-15 14:46:11 +0800

    初始化配置文件

--- react_take_out_food/webpack.config.dev.js
+++ react_take_out_food/webpack.config.dev.js
@@ -1,10 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const fs = require('fs');
 const srcRoot = path.resolve(__dirname, 'src');;
 const devPath = path.resolve(__dirname, 'dev');
 const pageDir = path.resolve(srcRoot, 'page');
-
 const mainFile = 'index.js';
+
+function getHtmlArray(entryMap) {
+    let htmlArray = [];
+    Object.keys(entryMap).forEach((key) => {
+        let fullPathName = path.resolve(pageDir, key);
+        let fileName = path.resolve(fullPathName, key + '.html');
+
+        if (fs.existsSync(fileName)) {
+            htmlArray.push(new HtmlWebpackPlugin({
+                filename: key + '.html',
+                template: fileName,
+                chunks: ['common', key]
+            }));
+        }
+
+
+    });
+    return htmlArray;
+}
 
 function getEntry() {
     let entryMap = {};
@@ -22,6 +41,7 @@ function getEntry() {
 }
 
 const entryMap = getEntry();
+const htmlArray = getHtmlArray(entryMap);
 
 module.exports = {
     mode: 'development',
@@ -32,15 +52,13 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.css$/, use: ['style-loader', 'css-loader'], include: srcRoot
-            },
-            {
-                test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'], include: srcRoot
-            },
-            {
-                test: /\.(png|jpg|jpeg)$/, use: 'url-loader?limit=8192', include: srcRoot
-            }
+            { test: /\.(js|jsx)$/, use: [{ loader: 'babel-loader' }], include: srcRoot },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'], include: srcRoot },
+            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'], include: srcRoot },
+            { test: /\.(png|jpg|jpeg)$/, use: 'url-loader?limit=8192', include: srcRoot }
         ]
-    }
+    },
+    plugins: [
+
+    ].concat(htmlArray)
 };
